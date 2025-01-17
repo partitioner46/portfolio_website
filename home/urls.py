76df6c6ff4b968933674bbaa54093fb957450a35
partitioner46 <samuel.e.shifflett@gmail.com>
2025-01-16 19:19:30 -0600

    blog, projects (github link), site name

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "home"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("tests", views.tests, name="tests"),
     path("about", views.about, name="about"),
     path("blog", views.blog, name="blog"),
     path("tech", views.tech, name="tech"),
--- home/views.py
+++ home/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+import requests as rq
+import os
+
+
+def get_repos():
+    repo_url = "https://api.github.com/users/partitioner46/repos"
+    raw_repos = rq.get(url=repo_url).json()
+    cleaned_repos = [{"name": repo['name'], "desc": repo['description'],
+                      "html_url": repo['html_url']} for repo in raw_repos]
+    print(cleaned_repos)
+    return cleaned_repos
 
 
 # home page url (will be a carousel for now)
@@ -23,4 +34,11 @@ def tech(request):
 
 
 def projects(request):
-    return render(request, 'home/projects.html')
+    """
+    TODO add the repo languages
+    https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-languages
+    """
+    repos = get_repos()
+    # access_token = os.getenv('GITHUB_API_TOKEN')
+    context = {'data': repos}
+    return render(request, 'home/projects.html', context=context)
